b55bdda00088d6f4ec920402733abdf702e22c84
aesarj <Ya3382509@gmail.com>
2025-05-21 06:58:26 -0700

    Update sit.py

--- AnonXMusic/plugins/bot/sit.py
+++ AnonXMusic/plugins/bot/sit.py
@@ -1,22 +1,32 @@
 from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import Message
 from AnonXMusic import app
 from strings.filters import command
-from config import SOURCE_PHOTO, DEV_URL, UPDATES_URL, SUPPORT_URL
+from config import OWNER_ID
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+
 
 @app.on_message(
-    command(["المالك", "مطور", "المطور"])
+    command(["مبرمج", "مطور", "المطور"])
 )
 async def mak(client: Client, message: Message):
     await message.reply_photo(
-        photo=SOURCE_PHOTO,
-        caption="𝐒𝐎𝗨𝐑𝐂𝐄 𝐏𝐀𝐑𝐈𝐒\n~ Dav .",
+        photo="https://envs.sh/Zow.jpg",
+        caption="𝐒𝐎𝗨𝐑𝐂𝐄 𝐏𝐀𝐑𝐈𝐒\n~ Dav Source",
         reply_markup=InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("⦗ Dev ⦘", url="https://t.me/F_Q_1")],
                 [
-                    InlineKeyboardButton("⦗ Updates ⦘", url="https://t.me/y_9ame"),
-                    InlineKeyboardButton("⦗ support ⦘", url="https://t.me/sorce_paris"),
+                    InlineKeyboardButton(
+                        "⦗ Dev ⦘", url="https://t.me/F_Q_1"
+                    ),
+                ],
+                [
+                    InlineKeyboardButton(
+                        "⦗ Updates ⦘", url="https://t.me/y_9ame"
+                    ),
+                    InlineKeyboardButton(
+                        "⦗ support ⦘", url="https://t.me/sorce_paris"
+                    ),
                 ],
             ]
         ),
