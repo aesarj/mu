c287f20bf8a371d53d409d00ca6a6f3d197773af
aesarj <Ya3382509@gmail.com>
2025-05-21 16:21:51 -0700

    Update play.py

--- AnonXMusic/plugins/play/play.py
+++ AnonXMusic/plugins/play/play.py
@@ -53,7 +53,7 @@ async def check_is_joined(message):
             "play",
             "تشغيل",
             "شغل",
-            "فيديو",
+            "فيد",
             "فديو",
             "cplay",
             "cvplay",
