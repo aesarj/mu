b77f42224b5932cc51530176a5b12c00267b317c
aesarj <Ya3382509@gmail.com>
2025-05-21 16:08:24 -0700

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/aesarj/mu",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", "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"
 )  # املأ هذا المتغير إذا كان مستودعك الأصلي خاصًا
